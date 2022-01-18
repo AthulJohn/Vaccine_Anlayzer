f5cc454bcf28dab7e3d8f88a41faeb54b1aefbf0
Kiran S Baliga <60563196+kiransbaliga@users.noreply.github.com>
2022-01-18 12:01:27 +0530

    page init

--- lib/vaccinehome.dart
+++ lib/vaccinehome.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:percent_indicator/percent_indicator.dart';
+import 'variables.dart';
 
 class VaccineHome extends StatefulWidget {
   VaccineHome({Key? key}) : super(key: key);
@@ -11,6 +12,171 @@ class VaccineHome extends StatefulWidget {
 class _VaccineHomeState extends State<VaccineHome> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+        title: const Text(
+          'Vaccine Analyzer',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(height: 30),
+            const Text(
+              'Current Vaccination Status',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey),
+            ),
+            const SizedBox(height: 35),
+            CircularPercentIndicator(
+              radius: 200,
+              lineWidth: 10.0,
+              animation: true,
+              percent: percent / 100,
+              progressColor: percent > 50 ? Colors.green : Colors.red,
+              center: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '$percent%',
+                    style: const TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black),
+                  ),
+                  const Text(
+                    'Vaccinated',
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 50),
+            Column(
+              children: [
+                Text(
+                  '$doses',
+                  style: const TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'Doses Taken',
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.grey),
+                ),
+              ],
+            ),
+            const SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      '$singdosecnt',
+                      style: const TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'People Vaccinated',
+                      style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.grey),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      '$fullycnt',
+                      style: const TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'People Fully Vaccinated',
+                      style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+/*
+Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                           Text(
+                            '$doses',
+                            style: const TextStyle(
+                                fontSize: 38,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black),
+                          ),
+                          const SizedBox(height: 10),
+                          const Text(
+                            'Doses Taken',
+                            style:  TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            '$singdosecnt',
+                            style: const TextStyle(
+                                fontSize: 38,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black),
+                          ),
+                          const SizedBox(height: 10),
+                          const Text(
+                            'People Vaccinated',
+                            style:  TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.grey),
+                          ),
+                        ],
+                      ),
+
+*/
